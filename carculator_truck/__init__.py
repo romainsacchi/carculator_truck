36378854f9e1d5f9152deef7183f03727e8f56ee
romainsacchi <r_s@me.com>
2020-03-28 16:11:55 +0100

    Repo creation

---
+++ carculator_truck/__init__.py
@@ -0,0 +1,21 @@
+"""
+
+Submodules
+==========
+
+.. autosummary::
+    :toctree: _autosummary
+
+
+"""
+
+
+
+__version__ = (0, 0, 1)
+
+from pathlib import Path
+
+DATA_DIR = Path(__file__).resolve().parent / "data"
+
+
+print("Hello!")
---
+++ setup.py
@@ -0,0 +1,66 @@
+from setuptools import setup
+import os
+
+packages = []
+root_dir = os.path.dirname(__file__)
+if root_dir:
+    os.chdir(root_dir)
+
+
+# Probably should be changed, __init__.py is no longer required for Python 3
+for dirpath, dirnames, filenames in os.walk("carculator_truck"):
+    # Ignore dirnames that start with '.'
+    if "__init__.py" in filenames:
+        pkg = dirpath.replace(os.path.sep, ".")
+        if os.path.altsep:
+            pkg = pkg.replace(os.path.altsep, ".")
+        packages.append(pkg)
+
+
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join("..", path, filename))
+    return paths
+
+
+setup(
+    name="carculator_truck",
+    version="0.0.1",
+    packages=packages,
+    author="Romain Sacchi <romain.sacchi@psi.ch>, Chris Mutel <christopher.mutel@psi.ch>",
+    license=open("LICENSE").read(),
+    package_data={"carculator_truck": package_files(os.path.join("carculator_truck", "data"))},
+    install_requires=[
+        "pandas",
+        "xarray",
+        "numpy",
+        "pytest",
+        "pytest-cov",
+        "klausen",
+        "xlrd",
+        "pytest",
+        "numexpr",
+        "bw2io",
+    ],
+    url="https://github.com/romainsacchi/carculator",
+    description="Prospective environmental and economic life cycle assessment of public transport and heavy goods vehicles",
+    classifiers=[
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "Topic :: Scientific/Engineering :: Visualization",
+    ],
+)
