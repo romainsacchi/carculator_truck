20aa74adec4582fe4e9263b0d9b740d69428ba2b
Randy Duodu <duodurandy19@gmail.com>
2023-06-09 16:49:15 +0000

    Updated conf.py and readthedocs.yml

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ import os
 import sys
 
 # sys.path.insert(0, os.path.abspath('C:\Users\sacchi_r\Documents\GitHub\coarse\coarse'))
-sys.path.insert(0, os.path.abspath(".."))
+sys.path.append(os.path.abspath(".."))
 
 
 # -- Project information -----------------------------------------------------
